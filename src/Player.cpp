623ea61c1307167e1315eee636bdc05f37747200
martonMeszaros <marton.m95@gmail.com>
2017-10-10 14:46:53 +0200

    Updated Player class: tracking room, changing health, picking up loot

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,9 +4,15 @@
 
 #include "../headers/Player.h"
 
-Player::Player():
+Player::Player(Room &startingRoom) :
+        // Magic numbers are arbitrary
         maxHealth(10),
         currentHealth(10),
-        inventory(100){
+        inventory(100),
+        currentRoom(&startingRoom) {
 
+}
+
+void Player::pickUpItem(Item &newItem) {
+    inventory.addItem(newItem);
 }
